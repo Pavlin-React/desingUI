849bf63b0a7d7695848d06be01ce6ea9c7c765e0
Pavlin Ivanov <pavlinivanov9@gmail.com>
2021-10-26 12:55:54 +0300

    Stack Navigation

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 let Stack = createNativeStackNavigator();
 
-let HomeScreen = () => {
+let HomeScreen = ({navigation}) => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Hello from Home Screen</Text>
+      <Button
+        title="Go to Details Screen"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+};
+
+let DetailsScreen = ({navigation}) => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Hello from Details Screen</Text>
+      <Button
+        title="Go to Home Screen"
+        onPress={() => navigation.navigate('Home')}
+      />
     </View>
   );
 };
@@ -16,8 +32,18 @@ let HomeScreen = () => {
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#009387',
+          },
+          headerTintColor: '#fff',
+          headerTintStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
+        <Stack.Screen name="Home" component={HomeScreen} options={{title: "Overview"}} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
