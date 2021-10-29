349304d02362533500fedca038e04a9ead66f41e
Pavlin Ivanov <pavlinivanov9@gmail.com>
2021-10-29 20:28:31 +0300

    Sign In UI

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,85 +1,113 @@
-import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native'
+import React from 'react';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  Platform,
+  TextInput,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+import * as Animatable from 'react-native-animatable';
 
 const SignInScreen = ({navigation}) => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Hello from Sign In Screen</Text>
-      <Button
-        title="Hi from SignIn Screen"
-        onPress={() => navigation.navigate('Home')}
-      />
+    <View style={styles.container}>
+      <View style={styles.header} >
+        <Text style={styles.text_header} >Welcome</Text>
+      </View>
+      <View style={styles.footer} >
+        <Text style={styles.text_footer} >
+          Email
+        </Text>
+        <View style={styles.action} >
+          <FontAwesome
+            name='user-o'
+            color='#05375a'
+            size={20}
+          />
+          <TextInput
+            placeholder='Your Email'
+            style={styles.textInput}
+            autoCapitalize='none'
+          />
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default SignInScreen
+export default SignInScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    backgroundColor: '#009387'
+    flex: 1,
+    backgroundColor: '#009387',
   },
   header: {
-      flex: 1,
-      justifyContent: 'flex-end',
-      paddingHorizontal: 20,
-      paddingBottom: 50
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+    paddingBottom: 50,
   },
   footer: {
-      flex: 3,
-      backgroundColor: '#fff',
-      borderTopLeftRadius: 30,
-      borderTopRightRadius: 30,
-      paddingHorizontal: 20,
-      paddingVertical: 30
+    flex: 3,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
   },
   text_header: {
-      color: '#fff',
-      fontWeight: 'bold',
-      fontSize: 30
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 30,
   },
   text_footer: {
-      color: '#05375a',
-      fontSize: 18
+    color: '#05375a',
+    fontSize: 18,
   },
   action: {
-      flexDirection: 'row',
-      marginTop: 10,
-      borderBottomWidth: 1,
-      borderBottomColor: '#f2f2f2',
-      paddingBottom: 5
+    flexDirection: 'row',
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#f2f2f2',
+    paddingBottom: 5,
   },
   actionError: {
-      flexDirection: 'row',
-      marginTop: 10,
-      borderBottomWidth: 1,
-      borderBottomColor: '#FF0000',
-      paddingBottom: 5
+    flexDirection: 'row',
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#FF0000',
+    paddingBottom: 5,
   },
   textInput: {
-      flex: 1,
-      marginTop: Platform.OS === 'ios' ? 0 : -12,
-      paddingLeft: 10,
-      color: '#05375a',
+    flex: 1,
+    marginTop: Platform.OS === 'ios' ? 0 : -12,
+    paddingLeft: 10,
+    color: '#05375a',
   },
   errorMsg: {
-      color: '#FF0000',
-      fontSize: 14,
+    color: '#FF0000',
+    fontSize: 14,
   },
   button: {
-      alignItems: 'center',
-      marginTop: 50
+    alignItems: 'center',
+    marginTop: 50,
   },
   signIn: {
-      width: '100%',
-      height: 50,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: 10
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
   },
   textSign: {
-      fontSize: 18,
-      fontWeight: 'bold'
-  }
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
 });
